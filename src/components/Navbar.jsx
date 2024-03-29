e09e905e177bef626e06f9eb4aa54eb3f23c7d7d
Pritam Paul <119868451+pritampaul02@users.noreply.github.com>
2024-03-29 21:11:21 +0530

    Ahin -

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,38 @@
-import {Link} from 'react-router-dom'
+import { Link, useLocation } from "react-router-dom";
+
+import profile from "../media/image.png";
+
+import "../style/navbar.css";
 
 const Navbar = () => {
-  return (
-    <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-    </div>
-  )
-}
+    const activeLocation = useLocation().pathname;
 
-export default Navbar
+    return (
+        <header className="navber">
+            <Link to="/" className="logo">
+                Logo
+            </Link>
+            {/* <div className="menu"> */}
+            <div className="menuContainer">
+                <Link
+                    to="/"
+                    className={activeLocation === "/" ? "menuActive" : ""}
+                >
+                    Home
+                </Link>
+                <Link
+                    to="/about"
+                    className={activeLocation === "/about" ? "menuActive" : ""}
+                >
+                    About
+                </Link>
+                <Link className="profileContainer" to="/">
+                    <img src={profile} alt="" />
+                </Link>
+            </div>
+            {/* </div> */}
+        </header>
+    );
+};
+
+export default Navbar;
